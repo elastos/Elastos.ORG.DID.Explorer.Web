e042fbb9e6162b08b8681d75830f794c49ee9eb0
zhangml123 <s15532292192@163.com>
2019-05-22 11:47:30 +0800

    Fix some bugs

---
+++ src/components/elements/Clipboard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ClipboardJS from 'clipboard';
+class Clipboard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            status : null
+        }
+    }
+	componentDidMount (){
+        const self = this;
+        const clipboard = new ClipboardJS('.btn');
+        clipboard.on('success', function(e) {
+            self.setState({
+                status:"success"
+            })
+            setTimeout(function(){
+                self.setState({
+                    status:null
+                })     
+            },1000)
+            e.clearSelection();
+        });
+        clipboard.on('error', function(e) {
+            self.setState({
+                status:"error"
+            })
+        });
+    }
+    render() {
+    	const eleId = this.props.eleId;
+    	const icon = this.props.icon;
+    	const style = this.props.style;
+        const status = this.state.status;
+        return (
+            <div style={{"display":"inline-block"}}>
+                <img className="btn" data-clipboard-target={"#"+eleId} src={icon} title = "copy" style = {style} alt="Copy to clipboard"/>
+                {status === "success" && <span style={{"fontSize":"12px","color":"#00cc99","marginLeft":"5px"}}>复制成功 ！</span>}
+                {status === "error" && <span style={{"fontSize":"12px","color":"#cc0099","marginLeft":"5px"}}>复制失败 ！</span>}
+            </div>
+        );
+    }
+}
+
+export default Clipboard;
